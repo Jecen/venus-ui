b44a83f483730aaac7a782d98261ff5661705ec9
ousututu <xiawenjuan@ousututudeMacBook-Pro.local>
2018-05-31 10:59:47 +0800

    update

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 module.exports = {
+  mode: "production",
   entry: './index.js',
   output: {
     filename: 'venus-ui.js',
--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 import LCDNumber from './src/lcd-number.js'
 
+const onload = function () {
+    // For native Imports, manually fire WebComponentsReady so user code
+    // can use the same code path for native and polyfill'd imports.
+    if (!window.HTMLImports) {
+        document.dispatchEvent(
+            new CustomEvent('WebComponentsReady', {
+                bubbles: true,
+            })
+        );
+    }
+};
+const webComponentsSupported = (
+    'registerElement' in document &&
+    'import' in document.createElement('link') &&
+    'content' in document.createElement('template')
+);
+console.log(webComponentsSupported, '-=-=-=')
+if (!webComponentsSupported) {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = '//unpkg.com/@webcomponents/webcomponentsjs@next/webcomponents-loader.js';
+    script.onload = onload;
+    document.head.appendChild(script);
+} else {
+    onload();
+}
+
 export default {
     'lcd-number': LCDNumber
 }
